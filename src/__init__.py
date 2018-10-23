68f334fcce924a1a141809d694bffaf8aa100bb5
Joao Paulo Monteiro Cruvinel da Costa <joao.cruvinel@embraer.com.br>
2018-10-23 15:34:16 -0300

    Reorganization

---
+++ src/__init__.py
@@ -0,0 +1,7 @@
+from . import aerodynamics
+from . import aeroelasticity
+from . import control
+from . import flight_mechanics
+from . import geometry
+from . import structures
+from . import visualization
